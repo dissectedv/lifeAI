9b2896b84caddbef0d02151627ca8c92823200c8
dissectedv <joaovictorsampaio393@gmail.com>
2025-04-27 03:50:33 -0400

    main_page/melhoria index

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include  # Adicione 'include'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lifeAI.urls')),  # Inclui as URLs do app lifeAI
-]
+] 
--- lifeAI/views.py
+++ lifeAI/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings 
 
 def index(request):
     return render(request, 'lifeAI/index.html')
+
+def main_page(request):
+    return render(request, 'lifeAI/main_page.html', {'DEBUG': settings.DEBUG})
