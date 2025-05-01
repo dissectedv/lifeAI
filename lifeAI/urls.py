672274fd8de9c19a42fd0e05e569706652a94f58
Guilherme Silva <guilhermesilvarosa92@gmail.com>
2025-05-01 15:43:57 -0400

    registro de conta, login e logout realizado

--- lifeAI/urls.py
+++ lifeAI/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register_view, name='register_view'), 
+    path('', views.register_view, name='register_view'),
     path('login/', views.login_view, name='login_view'),
+    path('', views.logout_view, name='logout'),
     path('main/', views.main_page, name='main_page'),
 ]
--- lifeAI/views.py
+++ lifeAI/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
+
 def register_view(request):
-    if request.method == 'POST':    
+    if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -15,14 +16,16 @@ def register_view(request):
             elif User.objects.filter(email=email).exists():
                 messages.error(request, 'Email já está em uso.')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 login(request, user)
                 messages.success(request, 'Usuário registrado com sucesso!')
-                return redirect('main_page')
+                return redirect('main_page.html')
         else:
             messages.error(request, 'Todos os campos são obrigatórios.')
 
     return render(request, 'lifeAI/index.html')
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -41,6 +44,13 @@ def login_view(request):
             messages.error(request, 'Usuário não encontrado.')
     return redirect('register_view')
 
-def main_page(request):
-    return render(request, 'lifeAI/main_page.html')
 
+def main_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login_view')
+    return render(request, "LifeAI/main_page.html", {'user': request.user})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('register_view')
